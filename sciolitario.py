3f1bd41cd959e3b17420b136e3eb92d48e52ecd1
M1keCodingProjects <mikif.2002@gmail.com>
2025-07-19 19:03:45 +0200

    Changed user input commands to speed up game. - Added intro game description - Fixed Ace rank in every visualization: from "1" to "A" - User input is now a single-level system for fast 2 card pairing and deck drawing.

--- sciolitario.py
+++ sciolitario.py
@@ -11,15 +11,15 @@ class Suit(StrEnum):
 
 type Rank = int
 def intoRank(n:int) -> Rank:
-    if n < 1 or n > 10: raise ValueError(f"A Rank must be between 1 and 10, got {n} instead.")
+    if n < 1 or n > 10: raise ValueError(f"ranks must be between 1 and 10, got {n} instead.")
 
     return n
 
 class Card:
     TOP        = "┌───┐"
     BOTTOM     = "└───┘"
-    FACES      = "JQK"
-    FACE_NAMES = ("Jack", "Queen", "King")
+    FACES      = "JQKA"
+    FACE_NAMES = ("Jack", "Queen", "King", "Ace")
 
     def __init__(self, suit:Suit, rank:Rank) -> None:
         self.suit      = suit
@@ -34,7 +34,11 @@ class Card:
         return self.suit == suit and self.rank == rank
 
     def _getRankStr(self, *, asSymbol = True) -> str:
-        return str(self.rank) if self.rank < 8 else (Card.FACES if asSymbol else Card.FACE_NAMES)[self.rank - 8]
+        faces = Card.FACES if asSymbol else Card.FACE_NAMES
+        match self.rank:
+            case 1:                      return faces[3]
+            case int() if self.rank > 7: return faces[self.rank - 8]
+            case _:                      return str(self.rank)
 
     def getCardLayers(self:Optional[Self]) -> tuple[str, str, str, str]:
         if not self: return ("     ", ) * 4
@@ -203,16 +207,15 @@ class Table:
         print(buff + '\n'.join(layers))
 
 class GameManager:
-    class InvalidCardErr(Exception):
-        def __init__(self, cardInpt:str, details = "") -> None:
-            super().__init__("Invalid card selection" +
-                f": {details}" * bool(details) +
-                f", got \"{cardInpt}\" instead.")
+    class InvalidActionErr(Exception):
+        BELOW_TOP_MSG = "cannot select discarded cards below the top of the pile"
 
-    class InvalidPairErr(Exception):
-        DEF_DETAILS = "the ranks add up to {}, not 10"
-        def __init__(self, first:Card, second:Card, details = DEF_DETAILS) -> None:
-            super().__init__(f"Invalid pair attempt between {first.name} and {second.name}, {details.format(first.rank + second.rank)}.")
+        class ActionType(StrEnum):
+            CardSelection = "card selection"
+            Pairing       = "pairing of cards"
+        
+        def __init__(self, action:ActionType, details = "") -> None:
+            super().__init__(f"Invalid {action}" + f": {details}" * bool(details) + '.')
 
     def __init__(self) -> None:
         self.deck  = Deck(filled = True)
@@ -224,6 +227,15 @@ class GameManager:
     
     def run(self) -> None:
         self.show()
+        print(f"""
+Welcome to solitaire! Your goal is to clear the table by making pairs.
+Any 2 cards with ranks adding up to exactly 10 can form a pair.
+Jacks are worth 8, Queens 9 and Kings 10 (Aces 1).
+The deck is a standard 40-cards european deck, so no jokers.
+Specify cards to pair as "RS RS", where R is the rank (1-10 or J, Q, K, A)
+and S is the suit: H for Hearts({Suit.Hearts}), S for Spades({Suit.Spades}), C for Clubs({Suit.Clubs}) and D for Diamonds({Suit.Diamonds}).
+Good luck!
+""")
         try:
             while self.table.nRows:
                 self.update()
@@ -237,20 +249,14 @@ class GameManager:
     
     def update(self) -> None:
         while True:
-            match input("Would you like to: draw & discard (d) or pair 2 cards (p)? ").lower().strip():
-                case 'd':
-                    self.drawAndDiscard()
-                    break
-                
-                case 'p':
-                    try:
-                        self.pairCards()
-                        break
+            userInpt = input("Draw & discard (d) or specify cards to pair: ").strip().lower()
+            if not userInpt: continue
 
-                    except (GameManager.InvalidCardErr, GameManager.InvalidPairErr) as err: print(err)
-                
-                case _  :
-                    print("Invalid action, input either 'd' or 'p'.")
+            if userInpt == 'd':
+                self.drawAndDiscard(); break
+
+            try: self.pairCards(userInpt); break
+            except GameManager.InvalidActionErr as err: print(err)
 
     def drawAndDiscard(self) -> None:
         self.pile.put(self.deck.draw())
@@ -260,42 +266,62 @@ class GameManager:
         self.comp.put(card)
         card.isCovered = True
 
-    def pairCards(self) -> None:
+    def pairCards(self, userInpt:str) -> None:
+        userInpts = userInpt.split()
+
         self.isPile2ndSelected = False
-        if (first := self.selectCard()).rank == 10:
-            if self.isPile2ndSelected: raise GameManager.InvalidCardErr(
-                first.name, "cannot select discarded cards below the top")
+        if (first := self.selectCard(userInpts[0])).rank == 10:
+            if self.isPile2ndSelected: raise GameManager.InvalidActionErr(
+                GameManager.InvalidActionErr.ActionType.CardSelection,
+                GameManager.InvalidActionErr.BELOW_TOP_MSG)
             
             self._removeCard(first)
             return
 
         firstIsPile2nd = self.isPile2ndSelected
 
-        second = self.selectCard()
-        if not first.canPair(second): raise GameManager.InvalidPairErr(first, second)
-        if first is second: raise GameManager.InvalidPairErr(first, second, "cannot pair a card with itself")
+        if len(userInpts) < 2: raise GameManager.InvalidActionErr(
+            GameManager.InvalidActionErr.ActionType.Pairing,
+            "only kings can be eliminated as single cards")
+        
+        if len(userInpts) > 2: raise GameManager.InvalidActionErr(
+            GameManager.InvalidActionErr.ActionType.Pairing,
+            "more than 2 cards were selected")
+
+        second = self.selectCard(userInpts[1])
+        if not first.canPair(second): raise GameManager.InvalidActionErr(
+            GameManager.InvalidActionErr.ActionType.Pairing,
+            f"the sum of these cards' ranks is {first.rank + second.rank}, not 10")
+        
+        if first is second: raise GameManager.InvalidActionErr(
+            GameManager.InvalidActionErr.ActionType.Pairing, "cannot pair a card with itself")
         
         if firstIsPile2nd: # 2nd check should only be done if first card is not pile 2nd
-            if second is not self.pile.cards[-1]: raise GameManager.InvalidCardErr(
-                first.name, "cannot select discarded cards below the top")
+            if second is not self.pile.cards[-1]: raise GameManager.InvalidActionErr(
+                GameManager.InvalidActionErr.ActionType.CardSelection,
+                GameManager.InvalidActionErr.BELOW_TOP_MSG)
         
         elif self.isPile2ndSelected and first is not self.pile.cards[-1]:
-            raise GameManager.InvalidCardErr(second.name, "cannot select discarded cards below the top")
+            raise GameManager.InvalidActionErr(
+                GameManager.InvalidActionErr.ActionType.CardSelection,
+                GameManager.InvalidActionErr.BELOW_TOP_MSG)
 
         self._removeCard(first)
         self._removeCard(second)
     
-    def selectCard(self) -> Card:
-        userInpt = input("Insert rank (1-10) and suit (H, S, C or D) of selected card: ").strip()
-        patMatch = search(r"^(\d\d?|j|q|k) ?(h|s|c|d)$", userInpt.lower())
-
-        if not patMatch: raise GameManager.InvalidCardErr(userInpt, "could not identify valid suit and rank")
+    def selectCard(self, userInpt:str) -> Card:
+        patMatch = search(r"^(\d\d?|j|q|k|a)(h|s|c|d)$", userInpt)
+        if not patMatch: raise GameManager.InvalidActionErr(
+            GameManager.InvalidActionErr.ActionType.CardSelection,
+            f"could not identify valid suit and rank in \"{userInpt}\"")
 
         suit = Suit[Suit._member_names_["shdc".find(patMatch.group(2))]]
-        if (rank := patMatch.group(1)).isalpha(): rank = 8 + Card.FACES.find(rank.upper())
+        if (rank := patMatch.group(1)).isalpha():
+            rank = 1 if rank == 'a' else 8 + Card.FACES.find(rank.upper())
         else:
             try: rank = intoRank(int(rank))
-            except ValueError as err: raise GameManager.InvalidCardErr(userInpt, str(err))
+            except ValueError as err: raise GameManager.InvalidActionErr(
+                GameManager.InvalidActionErr.ActionType.CardSelection, str(err))
 
         if card := self.pile.select(suit, rank): # condition split for length
             if len(self.pile.cards) > 1 and card is self.pile.cards[-2]: self.isPile2ndSelected = True
@@ -303,7 +329,9 @@ class GameManager:
         if not card: card = self.table.select(suit, rank)
         if card: return card
 
-        raise GameManager.InvalidCardErr(userInpt, "The selected card is not available")
+        raise GameManager.InvalidActionErr(
+            GameManager.InvalidActionErr.ActionType.CardSelection,
+            f"The selected card ({Card(suit, rank).name}) is not available")
 
     def show(self) -> None:
         self.table.print()
